536bdf384c9d4e4191d207ed8500665bd19ac3df
Aadarsha Shrestha <shresthaaadarsha4321@gmail.com>
2025-11-10 23:05:37 +0545

    Update q11.c

--- src/q11.c
+++ src/q11.c
@@ -1,1 +1,26 @@
-// Write a function named calculateAverage that takes an array of integers as input and returns the average of the numbers.
+#include <stdio.h>
+
+double calculateAverage(int arr[], int size) {
+    int sum = 0;
+    for(int i = 0; i < size; i++) {
+        sum += arr[i];
+    }
+    return (double)sum / size;
+}
+
+int main() {
+    int n;
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int numbers[n];
+    for(int i = 0; i < n; i++) {
+        printf("Enter element %d: ", i + 1);
+        scanf("%d", &numbers[i]);
+    }
+
+    double avg = calculateAverage(numbers, n);
+    printf("Average: %.2lf\n", avg);
+
+    return 0;
+}
