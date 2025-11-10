6d49299ecf2dbefcdcbc5f3dab1fb93c40ab47fb
Aadarsha Shrestha <shresthaaadarsha4321@gmail.com>
2025-11-10 23:15:33 +0545

    Implement recursive string reversal function

--- src/q26.c
+++ src/q26.c
@@ -1,1 +1,19 @@
-// Write a recursive function named reverseString that takes a string as input and returns the reversed string.
+#include <stdio.h>
+#include <string.h>
+
+void reverseString(char str[], int start, int end) {
+    if(start >= end) return;
+    char temp = str[start];
+    str[start] = str[end];
+    str[end] = temp;
+    reverseString(str, start + 1, end - 1);
+}
+
+int main() {
+    char str[100];
+    scanf("%s", str);
+    int len = strlen(str);
+    reverseString(str, 0, len - 1);
+    printf("%s\n", str);
+    return 0;
+}
