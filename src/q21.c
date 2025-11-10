115bd82ac5f5fdf466d08ad8f4e39e52a498c961
Aadarsha Shrestha <shresthaaadarsha4321@gmail.com>
2025-11-10 23:12:47 +0545

    Implement recursive factorial function in C

--- src/q21.c
+++ src/q21.c
@@ -1,1 +1,14 @@
-// Write a recursive function named calculateFactorial that takes an integer n as input and returns its factorial.
+#include <stdio.h>
+
+long long calculateFactorial(int n) {
+    if(n == 0 || n == 1)
+        return 1;
+    return n * calculateFactorial(n - 1);
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    printf("%lld\n", calculateFactorial(n));
+    return 0;
+}
