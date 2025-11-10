31cb5d5a11bb6b53ba12af1fff54de8b6d1bc785
Aadarsha Shrestha <shresthaaadarsha4321@gmail.com>
2025-11-10 23:15:09 +0545

    Implement recursive sum of digits function

--- src/q25.c
+++ src/q25.c
@@ -1,1 +1,13 @@
-// Write a recursive function named calculateSumOfDigits that takes an integer n as input and returns the sum of its digits.
+#include <stdio.h>
+
+int calculateSumOfDigits(int n) {
+    if(n == 0) return 0;
+    return (n % 10) + calculateSumOfDigits(n / 10);
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    printf("%d\n", calculateSumOfDigits(n));
+    return 0;
+}
