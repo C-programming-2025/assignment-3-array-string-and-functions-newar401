43646fbdf0b953d81f1a67d9ef870f4c0e5dc9fb
Aadarsha Shrestha <shresthaaadarsha4321@gmail.com>
2025-11-10 23:09:58 +0545

    Implement reverseArray function to reverse an array

--- src/q16.c
+++ src/q16.c
@@ -1,1 +1,28 @@
-// Write a function named reverseArray that takes an array of integers as input and reverses the order of the elements in the array.
+#include <stdio.h>
+
+void reverseArray(int arr[], int n) {
+    int temp;
+    for(int i = 0; i < n / 2; i++) {
+        temp = arr[i];
+        arr[i] = arr[n - i - 1];
+        arr[n - i - 1] = temp;
+    }
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    reverseArray(arr, n);
+
+    for(int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
