77dc47e2e87511949baf249b25919c88a27b26f1
Aadarsha Shrestha <shresthaaadarsha4321@gmail.com>
2025-11-10 23:13:43 +0545

    Implement recursive GCD calculation function

--- src/q23.c
+++ src/q23.c
@@ -1,1 +1,13 @@
-// Write a recursive function named calculateGCD that takes two integers a and b as input and returns their greatest common divisor (GCD).
+#include <stdio.h>
+
+int calculateGCD(int a, int b) {
+    if(b == 0) return a;
+    return calculateGCD(b, a % b);
+}
+
+int main() {
+    int x, y;
+    scanf("%d %d", &x, &y);
+    printf("%d\n", calculateGCD(x, y));
+    return 0;
+}
