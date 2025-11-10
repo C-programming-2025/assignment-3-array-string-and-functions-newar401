87433a4e82b20bd1966fb5808926d3acef7bd6ad
Aadarsha Shrestha <shresthaaadarsha4321@gmail.com>
2025-11-10 23:20:04 +0545

    Add character counting program in C
    Implement a C program to count character occurrences in a string.

--- src/q9.c
+++ src/q9.c
@@ -1,1 +1,12 @@
-// Implement a C program to count the occurrence of a specific character in a string.
+#include <stdio.h>
+
+int main() {
+    char str[100], ch;
+    scanf("%s %c", str, &ch);
+    int count = 0;
+    for(int i = 0; str[i] != '\0'; i++) {
+        if(str[i] == ch) count++;
+    }
+    printf("%d\n", count);
+    return 0;
+}
