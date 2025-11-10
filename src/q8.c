c7e39471024763edc7aa78f908506ee5c55d4477
Aadarsha Shrestha <shresthaaadarsha4321@gmail.com>
2025-11-10 23:19:41 +0545

    Add palindrome checking program in C
    Implement a C program to check for palindromes.

--- src/q8.c
+++ src/q8.c
@@ -1,1 +1,19 @@
-// Write a C program to check if a given string is a palindrome.
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[100];
+    scanf("%s", str);
+    int len = 0;
+    while(str[len] != '\0') len++;
+    int flag = 1;
+    for(int i = 0; i < len / 2; i++) {
+        if(str[i] != str[len - i - 1]) {
+            flag = 0;
+            break;
+        }
+    }
+    if(flag) printf("Palindrome\n");
+    else printf("Not Palindrome\n");
+    return 0;
+}
