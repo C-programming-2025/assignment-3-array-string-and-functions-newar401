70f87e3252e11a808dbacb62d75c2fc56083854a
Aadarsha Shrestha <shresthaaadarsha4321@gmail.com>
2025-11-10 23:07:59 +0545

    Add factorial calculation function and main program
    Implement findFactorial function to calculate factorial of a number.

--- src/q13.c
+++ src/q13.c
@@ -1,1 +1,23 @@
-// Write a function named findFactorial that takes an integer as input and returns its factorial.
+#include <stdio.h>
+
+long long findFactorial(int n) {
+    long long factorial = 1;
+    for(int i = 1; i <= n; i++) {
+        factorial *= i;
+    }
+    return factorial;
+}
+
+int main() {
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if(num < 0) {
+        printf("Factorial is not defined for negative numbers.\n");
+    } else {
+        printf("Factorial of %d is %lld\n", num, findFactorial(num));
+    }
+
+    return 0;
+}
