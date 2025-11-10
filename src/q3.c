1a1fea10e8debd97268c58dd0d157221a787a546
Aadarsha Shrestha <shresthaaadarsha4321@gmail.com>
2025-11-10 23:17:15 +0545

    Add bubble sort implementation in q3.c
    Implement bubble sort to sort an array of integers.

--- src/q3.c
+++ src/q3.c
@@ -1,1 +1,27 @@
-// Write a C program to sort an array of integers in ascending order using a sorting algorithm of your choice (e.g., bubble sort, selection sort, insertion sort).
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    int arr[n], temp;
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    for(int i = 0; i < n - 1; i++) {
+        for(int j = 0; j < n - i - 1; j++) {
+            if(arr[j] > arr[j + 1]) {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+
+    for(int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
