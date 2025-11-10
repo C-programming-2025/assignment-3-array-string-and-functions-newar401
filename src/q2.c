5fe3199e0e900ef3c00b5b9cd0f5aa5a0c2172b6
Aadarsha Shrestha <shresthaaadarsha4321@gmail.com>
2025-11-10 23:11:56 +0545

    Update q2.c

--- src/q2.c
+++ src/q2.c
@@ -1,1 +1,20 @@
-// Implement a C program to reverse the elements of an array.
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    int arr[n], temp;
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    for(int i = 0; i < n / 2; i++) {
+        temp = arr[i];
+        arr[i] = arr[n - i - 1];
+        arr[n - i - 1] = temp;
+    }
+    for(int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+    return 0;
+}
