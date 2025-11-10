85f237d266fa58e0b6c897ceb2a9b53a1dfc15b2
Aadarsha Shrestha <shresthaaadarsha4321@gmail.com>
2025-11-10 23:11:30 +0545

    Implement factorial series calculation function

--- src/q19.c
+++ src/q19.c
@@ -1,1 +1,23 @@
-//  Write a function named calculateFactorialSeries that takes an integer n as input and prints the factorial series up to n.
+#include <stdio.h>
+
+long long factorial(int n) {
+    long long fact = 1;
+    for(int i = 1; i <= n; i++) {
+        fact *= i;
+    }
+    return fact;
+}
+
+void calculateFactorialSeries(int n) {
+    for(int i = 1; i <= n; i++) {
+        printf("%lld ", factorial(i));
+    }
+    printf("\n");
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    calculateFactorialSeries(n);
+    return 0;
+}
